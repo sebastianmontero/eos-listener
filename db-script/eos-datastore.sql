03eff45093319e4e11f577fa5a9d1338c5675a60
Sebastian Montero <smontero@mightywizards.com>
2018-12-11 13:54:00 -0600

    Updated database structure, added account_type, account, action, channel, order_type, token, and exchange_trades tables and their relationships

--- db-script/eos-datastore.sql
+++ db-script/eos-datastore.sql
@@ -1,3 +1,5 @@
+use eos_datastore;
+
 CREATE OR REPLACE TABLE time_type (
   time_type_id number(2,0) NOT NULL,
   time_type_desc varchar(50) NOT NULL,
@@ -103,23 +105,99 @@ VALUES (-4,'Hasn\'t Happened'),
        (-2,'Not Applicable'),
        (-1,'Unknown'),
        (0,'Normal');
+       
 
-CREATE TABLE exchange_trades (
-  id int NOT NULL AUTO_INCREMENT,
-  account varchar(50) NOT NULL,
-  action varchar(50) NOT NULL,
-  from varchar(50) NOT NULL,
-  to varchar(50) NOT NULL,
-  quantity varchar(50) NOT NULL,
-  ordertype varchar(50) DEFAULT NULL,
-  pair varchar(50) DEFAULT NULL,
-  trade_quantity varchar(50) DEFAULT NULL,
-  trade_price varchar(50) DEFAULT NULL,
-  channel varchar(50) DEFAULT NULL,
-  day_id mediumint(9) NOT NULL,
-  hour_of_day tinyint(4) NOT NULL,
-  block_time datetime NOT NULL DEFAULT '2000-01-01 00:00:00',
+CREATE OR REPLACE TABLE account_type (
+  account_type_id number(2,0) NOT NULL,
+  account_type_desc varchar(50) NOT NULL,
+  PRIMARY KEY (account_type_id)
+);
+
+INSERT INTO account_type (account_type_id, account_type_desc)
+VALUES (-1, 'Unknown'),
+       (1, 'Token'),
+       (2, 'Exchange'),
+       (3, 'Exchange');
+
+CREATE OR REPLACE TABLE account (
+  account_id int NOT NULL AUTOINCREMENT,
+  account_name varchar(50) NOT NULL,
+  account_type_id number(2,0) NOT NULL,
+  PRIMARY KEY (account_id),
+  CONSTRAINT fk_account_account_type FOREIGN KEY (account_type_id) REFERENCES  account_type (account_type_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+INSERT INTO account (account_id, account_name, account_type_id)
+VALUES (-1, 'Unknown', -1);
+
+
+CREATE OR REPLACE TABLE action (
+  action_id int NOT NULL AUTOINCREMENT,
+  action_name varchar(50) NOT NULL,
+  PRIMARY KEY (action_id)
+);
+
+CREATE OR REPLACE TABLE channel (
+  channel_id number(2,0) NOT NULL,
+  channel_name varchar(50) NOT NULL,
+  PRIMARY KEY (channel_id)
+);
+
+INSERT INTO channel (channel_id, channel_name)
+VALUES (-1, 'Unknown');
+
+
+CREATE OR REPLACE TABLE order_type (
+  order_type_id number(2,0) NOT NULL,
+  order_type_name varchar(50) NOT NULL,
+  PRIMARY KEY (order_type_id)
+);
+
+INSERT INTO order_type (order_type_id, order_type_name)
+VALUES (-1, 'Unknown'),
+       (1, 'buy'),
+       (2, 'buy-limit'),
+       (3, 'sell'),
+       (4, 'sell-limit'),
+       (5, 'cancel');
+
+CREATE OR REPLACE TABLE token (
+  token_id int NOT NULL AUTOINCREMENT,
+  token_name varchar(50) NOT NULL,
+  account_id int NOT NULL,
+  PRIMARY KEY (token_id),
+  CONSTRAINT fk_token_account FOREIGN KEY (account_id) REFERENCES  account (account_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+INSERT INTO token (token_id, token_name, account_id)
+VALUES (-1, 'Unknown', -1);
+
+CREATE OR REPLACE TABLE exchange_trades (
+  id int NOT NULL AUTOINCREMENT,
+  token_account_id int NOT NULL,
+  action_id int NOT NULL,
+  from_account_id int NOT NULL,
+  to_account_id int NOT NULL,
+  quantity NUMBER(22, 7) NOT NULL,
+  quantity_token_id int NOT NULL,
+  order_type_id NUMBER(2, 0) NOT NULL,
+  quote_token_id int NOT NULL,
+  base_token_id int NOT NULL,
+  trade_quantity NUMBER(22, 7),
+  trade_price NUMBER(22, 7),
+  channel_id NUMBER(2,0) NOT NULL,
+  day_id NUMBER(7, 0) NOT NULL,
+  hour_of_day NUMBER(2,0) NOT NULL,
+  block_time datetime NOT NULL,
   PRIMARY KEY (id),
-  KEY fk_exchange_trades_day_idx (day_id),
+  CONSTRAINT fk_exchange_trades_account1 FOREIGN KEY (token_account_id) REFERENCES account (account_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT fk_exchange_trades_action1 FOREIGN KEY (action_id) REFERENCES action (action_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT fk_exchange_trades_account2 FOREIGN KEY (from_account_id) REFERENCES account (account_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT fk_exchange_trades_account3 FOREIGN KEY (to_account_id) REFERENCES account (account_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT fk_exchange_trades_token1 FOREIGN KEY (quantity_token_id) REFERENCES token (token_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT fk_exchange_trades_order_type1 FOREIGN KEY (order_type_id) REFERENCES order_type (order_type_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT fk_exchange_trades_token2 FOREIGN KEY (quote_token_id) REFERENCES token (token_id) ON DELETE NO ACTION ON UPDATE NO ACTION,                                                                          
+  CONSTRAINT fk_exchange_trades_token3 FOREIGN KEY (base_token_id) REFERENCES token (token_id) ON DELETE NO ACTION ON UPDATE NO ACTION,                                                                                  
+  CONSTRAINT fk_exchange_trades_channel FOREIGN KEY (channel_id) REFERENCES channel (channel_id) ON DELETE NO ACTION ON UPDATE NO ACTION,                                                                                     
   CONSTRAINT fk_exchange_trades_day FOREIGN KEY (day_id) REFERENCES day (day_id) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
