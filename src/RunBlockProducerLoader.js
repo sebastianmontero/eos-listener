1178f5e73ebf9c0ac79486e4b302fe34dbb2facc
Sebastian Montero <smontero@mightywizards.com>
2019-01-08 16:42:27 -0600

    Incremented number of listeners limit for RunBlockProducerLoader

--- src/RunBlockProducerLoader.js
+++ src/RunBlockProducerLoader.js
@@ -1,7 +1,7 @@
 const BlockProducerLoader = require('./BlockProducerLoader');
 const config = require('config');
 const logger = require('./Logger');
-require('events').EventEmitter.defaultMaxListeners = 20;
+require('events').EventEmitter.defaultMaxListeners = 50;
 
 logger.configure('load-block-producers');
 const loader = new BlockProducerLoader(config);
