b43bf1946517b67f465e5cee02950c353e87df73
Sebastian Montero <smontero@mightywizards.com>
2019-01-11 20:41:28 -0600

    Changed log level to debug

--- src/RunBlockProducerLoader.js
+++ src/RunBlockProducerLoader.js
@@ -1,7 +1,7 @@
 const BlockProducerLoader = require('./BlockProducerLoader');
 const config = require('config');
 const logger = require('./Logger');
-require('events').EventEmitter.defaultMaxListeners = 50;
+require('events').EventEmitter.defaultMaxListeners = 10;
 
 logger.configure('load-block-producers');
 const loader = new BlockProducerLoader(config);
