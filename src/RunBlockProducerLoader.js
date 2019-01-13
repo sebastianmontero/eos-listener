4d6eb00d2a532d12fa2fc6d21bbf142ad04e4577
Sebastian Montero <smontero@mightywizards.com>
2019-01-12 18:10:19 -0600

    Removed limit on max listeners for BlockProducerLoader and VoterLoader. Configured the snapshot times for the blockProducer and voterSnapshots

--- src/RunBlockProducerLoader.js
+++ src/RunBlockProducerLoader.js
@@ -1,7 +1,7 @@
 const BlockProducerLoader = require('./BlockProducerLoader');
 const config = require('config');
 const logger = require('./Logger');
-require('events').EventEmitter.defaultMaxListeners = 10;
+require('events').EventEmitter.defaultMaxListeners = 0;
 
 logger.configure('load-block-producers');
 const loader = new BlockProducerLoader(config);
--- src/RunVoterLoader.js
+++ src/RunVoterLoader.js
@@ -1,7 +1,7 @@
 const VoterLoader = require('./VoterLoader');
 const config = require('config');
 const logger = require('./Logger');
-require('events').EventEmitter.defaultMaxListeners = 20;
+require('events').EventEmitter.defaultMaxListeners = 0;
 
 logger.configure('load-voters');
 const loader = new VoterLoader(config);
