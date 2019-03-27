e65b11d5e231f2b07c2da075b72bc56216ee4979
Sebastian Montero <smontero@mightywizards.com>
2019-03-27 17:54:21 -0600

    Initialized DBConnection in RunAccountBalanceLoader

--- src/RunAccountBalanceLoader.js
+++ src/RunAccountBalanceLoader.js
@@ -1,8 +1,10 @@
 const AccountBalanceLoader = require('./AccountBalanceLoader.js');
 const config = require('config');
 const logger = require('./Logger');
+const dbCon = require('./db/DBConnection');
 require('events').EventEmitter.defaultMaxListeners = 50;
 
 logger.configure('account-balance-loader');
+dbCon.init(config.db);
 const loader = new AccountBalanceLoader(config);
 loader.start();
