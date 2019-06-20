fe2a242244e768a9767c15730e91d5effd90fd02
Sebastian Montero <smontero@mightywizards.com>
2019-06-20 09:45:39 -0500

    Changed command from nodejs to node in the run-block-producer-loader script

--- run-block-producer-loader.sh
+++ run-block-producer-loader.sh
@@ -4,5 +4,5 @@ export NODE_ENV=$1
 echo $NODE_ENV
 cd "${BASH_SOURCE%/*}" || exit
 pushd src
-nodejs RunBlockProducerLoader.js > ../logs/load-block-producers-stdout.log 2> ../logs/load-block-producers-stderr.log &
+node RunBlockProducerLoader.js > ../logs/load-block-producers-stdout.log 2> ../logs/load-block-producers-stderr.log &
 popd
