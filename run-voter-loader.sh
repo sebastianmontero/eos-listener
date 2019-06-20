1cd5af4fcad7767b9856fbaf7d98635635a8ea8e
Sebastian Montero <smontero@mightywizards.com>
2019-06-20 09:41:28 -0500

    Changed command from nodejs to node in the run-voter-loader script

--- run-voter-loader.sh
+++ run-voter-loader.sh
@@ -4,5 +4,5 @@ export NODE_ENV=$1
 echo $NODE_ENV
 cd "${BASH_SOURCE%/*}" || exit
 pushd src
-nodejs RunVoterLoader.js > ../logs/load-voters-stdout.log 2> ../logs/load-voters-stderr.log &
+node RunVoterLoader.js > ../logs/load-voters-stdout.log 2> ../logs/load-voters-stderr.log &
 popd
